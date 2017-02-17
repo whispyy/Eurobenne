02244cc827d93949e9ccd6005d84f7a89a44a09f
JF <jf-durand@outlook.fr>
2017-02-17 18:08:37 +0100

    slider ok

---
+++ app/components/slider/slider.directive.js
@@ -0,0 +1,30 @@
+(function(){
+    'use strict';
+    
+    angular
+    .module('fjs.components')
+    .directive('slider', sliderDefinition);
+    
+    function sliderDefinition() {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/components/slider/slider.html',
+            scope: {
+                text: '=',
+                type: '='
+            },
+            controller: sliderDirectiveController,
+            controllerAs: 'vm',
+            bindToController: true
+        };
+    }
+    
+    function sliderDirectiveController() {
+        var vm = this;
+
+        //init the slider        
+        $('.carousel.carousel-slider').carousel({fullWidth: true});
+        
+    }
+    
+})();
