e26b5d8c79cca401cf9573b17faa1d0ee6e9206c
jfvm <jfdurk@gmx.fr>
2017-02-18 15:25:55 +0100

    debut implementation Benne page

--- app/bennes/bennes.controller.js
+++ app/bennes/bennes.controller.js
@@ -10,8 +10,9 @@
     function bennesController(bennes) {
         var vm = this;
         
-
+        vm.bennes3 = bennes.get3Bennes();
         vm.bennes = bennes.getBennes();
+        console.log(vm.bennes3);
         console.log(vm.bennes);
         
     }
--- app/services/bennes.service.js
+++ app/services/bennes.service.js
@@ -7,6 +7,36 @@
 
     function bennes(){
 
+        function get3Bennes(){
+            var bennes = [];
+            var b1 = 
+            {
+                nom: 'Benne Ouverte Eurostyle 33m³',
+                desc: '',
+                type: 'Benne Ouverte',
+                img: 'content/img/bennes/Benne_ouverte_eurostyle_33m.jpg'
+            };
+            var b2 =
+            {
+                nom: 'Benne Ouverte à Toles Lisses 25m³',
+                desc: '',
+                type: 'Benne Ouverte',
+                img: 'content/img/bennes/Benne_ouverte_toles_lisses_25m.jpg'
+            };
+            var b3 =
+            {
+                nom: 'Benne Ouverte à Toles Nervurées 20m³',
+                desc: '',
+                type: 'Benne Ouverte',
+                img: 'content/img/bennes/Benne_ouverte_toles_nervurees_20m.jpg'
+            };
+            bennes.push(b1);
+            bennes.push(b2);
+            bennes.push(b3);
+
+            return bennes;
+        }
+
         function getBennes(){
             var bennes = [];
             var b1 = 
@@ -14,22 +44,31 @@
                 nom: 'Benne Ouverte Eurostyle 33m³',
                 desc: '',
                 type: 'Benne Ouverte',
-                img: 'content/img/bennes/Benne_ouverte_eurostyle_33m'
+                img: 'content/img/bennes/Benne_ouverte_eurostyle_33m.jpg'
             };
             var b2 =
             {
                 nom: 'Benne Ouverte à Toles Lisses 25m³',
                 desc: '',
                 type: 'Benne Ouverte',
-                img: 'content/img/bennes/Benne_ouverte_toles_lisses_25m'
+                img: 'content/img/bennes/Benne_ouverte_toles_lisses_25m.jpg'
+            };
+            var b3 =
+            {
+                nom: 'Benne Ouverte à Toles Nervurées 20m³',
+                desc: '',
+                type: 'Benne Ouverte',
+                img: 'content/img/bennes/Benne_ouverte_toles_nervurees_20m.jpg'
             };
             bennes.push(b1);
             bennes.push(b2);
+            bennes.push(b3);
 
             return bennes;
         }
 
         var Services = {
+            get3Bennes: get3Bennes,
             getBennes: getBennes
         }
         return Services;
