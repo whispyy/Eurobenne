462879fe989ae92eb975da75627d185a28ab29a8
JF-Dell <jfdurk@gmx.fr>
2017-02-18 19:44:09 +0100

    contact form ok

--- app/contact/contact.controller.js
+++ app/contact/contact.controller.js
@@ -7,9 +7,29 @@
     
     function contactController($filter) {
         var vm = this;
+
+        //initialisations
+        $('.modal').modal();
+        emailjs.init("user_bohF04q3pAPOijmAfMD5u");
+
+        //stockage des informations du formulaire de contact
+        vm.contact = {};
         
-        vm.message = "Hello World !!";
-        
+        /*
+         * Envoie du formulaire
+         */
+        vm.send = function(){
+        	console.log(vm.contact);
+        	// parameters: service_id, template_id, template_parameters
+			emailjs.send("yandex","eurobenne",vm.contact)
+			.then(function(response) {
+			   console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+			   $('#modal1').modal('open');
+			}, function(err) {
+			   console.log("FAILED. error=", err);
+			});
+        	
+        }
     }
     
 })();
