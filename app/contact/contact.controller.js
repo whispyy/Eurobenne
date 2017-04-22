cfe96022c54f58e116cd6cc40ca4ee8a12ce8378
JF <jfdurk@gmx.fr>
2017-04-22 00:19:57 -0400

    feature(loader on comment send) - need to fix the loader refresh to stop showing it

--- app/contact/contact.controller.js
+++ app/contact/contact.controller.js
@@ -13,18 +13,21 @@
 
         //stockage des informations du formulaire de contact
         vm.contact = {};
+        vm.loading = false;
         
         /*
          * Envoie du formulaire
          */
         vm.send = function(){
-        	console.log(vm.contact);
+            vm.loading = true;
         	// parameters: service_id, template_id, template_parameters
 			emailjs.send("yandex","eurobenne",vm.contact)
 			.then(function(response) {
+               vm.loading = false;
 			   console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
 			   $('#modal1').modal('open');
 			}, function(err) {
+               vm.loading = false;
 			   console.log("FAILED. error=", err);
 			});
         	
