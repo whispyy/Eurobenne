1965c3d3a17cc63e91b752e615c113f7a8b53db5
JF <jfdurk@gmx.fr>
2017-05-07 14:59:17 -0400

    ES6 : accessoires services

--- app/services/accessoires.service.js
+++ app/services/accessoires.service.js
@@ -1,49 +1,50 @@
-(function(){
-    'use strict';
-    
-    angular
-    .module('fjs.services')
-    .service('accessoires', accessoires);
-
-    function accessoires(){
-        function getAccess(){
-            var accessoires = [];
-            var a1 = 
-            {
-                nom: 'Sabot d\'immobilisation',
-                desc: 'Sabot d\'immobilisation de benne verrouillable',
-                type: 'Accessoires',
-                img: 'content/img/accessoires/sabot.JPG'
-            };
-            var a2 =
-            {
-                nom: 'Anneau de préhension',
-                desc: '',
-                type: 'Accessoires',
-                img: 'content/img/accessoires/anneau.jpg'
-            };
-            var a3 =
-            {
-                nom: 'Cale de stockage',
-                desc: 'Permet l\'inclinaison de la benne et d\'éviter la corrosion ',
-                type: 'Accessoires',
-                img: 'content/img/accessoires/cale.jpg'
-            };
-            var a4 =
-            {
-                nom: 'Trappes sur la porte arrière',
-                desc: '',
-                type: 'Accessoires',
-                img: 'content/img/accessoires/trappesgrain.jpg'
-            };
-            accessoires.push(a1, a2, a3, a4);
-
-            return accessoires;
-        }
-
-        var Services = {
-            getAccess: getAccess
-        }
-        return Services;
+class Accessoires {
+    constructor() {
+        this.accessoires = [];
     }
-})();
+
+    getAccess(){
+        const a1 = 
+        {
+            nom: 'Sabot d\'immobilisation',
+            desc: 'Sabot d\'immobilisation de benne verrouillable',
+            type: 'Accessoires',
+            img: 'content/img/accessoires/sabot.JPG'
+        };
+        const a2 =
+        {
+            nom: 'Anneau de préhension',
+            desc: '',
+            type: 'Accessoires',
+            img: 'content/img/accessoires/anneau.jpg'
+        };
+        const a3 =
+        {
+            nom: 'Cale de stockage',
+            desc: 'Permet l\'inclinaison de la benne et d\'éviter la corrosion ',
+            type: 'Accessoires',
+            img: 'content/img/accessoires/cale.jpg'
+        };
+        const a4 =
+        {
+            nom: 'Trappes sur la porte arrière',
+            desc: '',
+            type: 'Accessoires',
+            img: 'content/img/accessoires/trappesgrain.jpg'
+        };
+        this.accessoires.push(a1, a2, a3, a4);
+
+        return this.accessoires;
+    }
+
+    accessoires() {
+        return {
+            getAccess: getAccess
+        };
+    }
+}
+
+    
+angular
+.module('fjs.services')
+.service('accessoires', Accessoires);
